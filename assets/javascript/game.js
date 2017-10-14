e0238fdaff46ac47c8be73e57456862041ff4428
Jason <vivanaranja@gmail.com>
2017-10-14 08:26:09 -0700

    bit of cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // variables
-// -------------------------------------
 
-var blueCrystal = 0;
-var redCrystal = 0;
-var greenCrystal = 0;
-var yellowCrystal = 0;
+
+var vaiJem = 0;
+var fenderEsquire = 0;
+var lesPaul = 0;
+var rickGlo = 0;
 
 var currentScore = 0;
 var targetScore = 0;
@@ -15,7 +15,7 @@ var lossCount = 0;
 
 
 //functions
-// -------------------------------------
+
 
 var getRandom = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -29,48 +29,42 @@ var startGame = function() {
     //set random target score
     targetScore = getRandom(19, 120);
 
-    //values for crystals
-    blueCrystal = getRandom(1, 12);
-    redCrystal = getRandom(1, 12);
-    greenCrystal = getRandom(1, 12);
-    yellowCrystal = getRandom(1, 12);
+    //values for guitars
+    vaiJem = getRandom(1, 12);
+    fenderEsquire = getRandom(1, 12);
+    lesPaul = getRandom(1, 12);
+    rickGlo = getRandom(1, 12);
 
 // use jQuery to insert html items
 	$("#yourScore").html(currentScore);
 	$("#targetScore").html(targetScore);
 
-    console.log("--------------------");
+
     console.log("Target Score: " + targetScore);
-    console.log("Blue: " + blueCrystal + " | Red: " + redCrystal + " | Green: " + greenCrystal + " | Yellow: " + yellowCrystal);
-    console.log("---------------------");
-}
-
-var addValues = function(crystal) {
-
-
-
-
+    console.log("JEM: " + vaiJem + " | Esquire: " + fenderEsquire + " | Les Paul: " + lesPaul + " | Rickenbacker: " + rickGlo);
 
 }
+
+
 
 
 //game logic
-// -------------------------------------
+
 
 startGame();
 
-$("#blue").click(function() {
-    alert("blue");
+$("#jem").click(function() {
+    alert("jem");
 });
 
-$("#red").click(function() {
-    alert("red");
+$("#esquire").click(function() {
+    alert("esquire");
 });
 
-$("#green").click(function() {
-    alert("green");
+$("#les").click(function() {
+    alert("les");
 });
 
-$("#yellow").click(function() {
-    alert("yellow");
+$("#rick").click(function() {
+    alert("rick");
 });

b9d42301eec47177f8a89c3126794cb23fd18f1e
Jason <vivanaranja@gmail.com>
2017-10-15 08:34:57 -0700

    wrestling the jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,24 @@
 // variables
 
-var vaiJem = 0;
-var fenderEsquire = 0;
-var lesPaul = 0;
-var rickGlo = 0;
+// var vaiJem = 0;
+// var fenderEsquire = 0;
+// var lesPaul = 0;
+// var rickGlo = 0;
 
-var yourScore = 0;
-var targetScore = 0;
+// var yourScore = 0;
+// var targetScore = 0;
 
 var winCount = 0;
 var lossCount = 0;
 
-//functions
+
 
 // get a random number for target score
 var getRandom = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// startGame function
+// startGame function to set up the game for play
 var startGame = function() {
 
     //reset score
@@ -43,8 +43,6 @@ var startGame = function() {
 
 //game logic
 
-
-
 startGame(); {
 
     //add calculated yourScore + guitar value and update on html 
@@ -52,41 +50,50 @@ startGame(); {
     $("#jem").click(function() {
         yourScore += vaiJem;
         $("#playerScore").html(yourScore);
-
+        console.log("Your Score: " + yourScore);
     });
 
     $("#esquire").click(function() {
         yourScore += fenderEsquire;
         $("#playerScore").html(yourScore);
+        console.log("Your Score: " + yourScore);
     });
 
     $("#les").click(function() {
         yourScore += lesPaul;
         $("#playerScore").html(yourScore);
+        console.log("Your Score: " + yourScore);
     });
 
     $("#rick").click(function() {
         yourScore += rickGlo;
         $("#playerScore").html(yourScore);
+        console.log("Your Score: " + yourScore);
     });
 
     //functions for winning or losing.
+    
+    /*This is not working. I can see that targetNumber and yourScore both display
+    and yourScore calculates. When it equals targetNumber...nothing. When it exceeds
+    targetNumber...nothing. No increase in winCount or lossCount.*/
 
-
-    if (yourScore == targetScore) {
+    if (yourScore === targetScore) {
         winCount++;
-    } else if (yourScore > targetScore) {
+        alert("you win");
+        startGame();
+    }
+
+    if (yourScore > targetScore) {
         lossCount++;
         alert("I'm sorry, but you're just too darn loud.");
-    } else {
-
-
+        startGame();
     }
 
 
     console.log("Target Score: " + targetScore);
-    console.log("Your Score: " + yourScore);
     console.log("JEM: " + vaiJem + " | Esquire: " + fenderEsquire + " | Les Paul: " + lesPaul + " | Rickenbacker: " + rickGlo);
     console.log("Your Wins: " + winCount);
     console.log("Your Losses: " + lossCount);
+
+
 };
